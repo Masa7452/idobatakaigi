c9f5d55027fb64e223148298ea4e4dc5bf171b2d
Masa7452 <masakazu@iwaharamasakazunoMacBook-Air.local>
2022-06-03 14:56:23 +0900

    create config

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import SignIn from './SignIn';
 import Main from './Main';
+import config from '../config.json';
 
 export default () => {
   const [name, setName] = useState('');
-  if(name === ''){
+  if(config.SignInEnabled && name === ''){
     return <SignIn setName={setName}/>;
   
   // 文字列が渡ってきた場合のみ、メイン画面に遷移する。
