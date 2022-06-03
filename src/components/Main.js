c7799adaaa7c4009b23b5a7a00457dcf7c4bd628
Masa7452 <masakazu@iwaharamasakazunoMacBook-Air.local>
2022-06-03 15:56:07 +0900

    apply-grid-layout

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Grid } from "@material-ui/core";
+import MessageInputField from "./MessageInputField";
+import MessageList from "./MessageList";
 
-const Main = () => {
-    return <div>Main</div>
+const useStyles = makeStyles({
+    root : {
+        // 要素を分割してくれる
+        display : 'grid',
+        // 画面いっぱいに使ってくれる。「%」は親要素に対する割合。「vh」はビューポート（画面サイズ）に対する割合
+        height : '100vh',
+        gridTemplateRows: '1fr auto'
+    }
+})
+
+const Main = ({name}) => {
+    const classes = useStyles();
+    return (
+        <div className={classes.root}>
+            <MessageList />
+            <MessageInputField name={name}/>
+        </div>
+    )
 }
 
 export default Main;
