d4fd2d6a1dc344d1770b049c87ead8f383195069
Masa7452 <masakazu@iwaharamasakazunoMacBook-Air.local>
2022-06-08 18:06:06 +0900

    generate-avatar

--- src/components/MessageInputField.js
+++ src/components/MessageInputField.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { Avatar, Grid } from "@material-ui/core";
+import { gravatarPath } from "../gravator";
 
 
 const useStyles = makeStyles({
@@ -10,13 +11,14 @@ const useStyles = makeStyles({
     },
 })
 
-const MessageInputField = () => {
+const MessageInputField = ({name}) => {
     const classes = useStyles();
+    const gravatar_path = gravatarPath(name)
     return (
         <div className={classes.root}>
             <Grid container>
                 <Grid xs={1}>
-                    <Avatar />
+                    <Avatar src={gravatar_path} />
                 </Grid>
                 <Grid xs={10}>入力</Grid>
                 <Grid xs={1}>ボタン</Grid>
--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -4,7 +4,6 @@ import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
-import Link from '@material-ui/core/Link';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Card from '@material-ui/core/Card';
