6d05f7aa1878f1510272123ab89f4fa363fb7782
Masa7452 <masakazu@iwaharamasakazunoMacBook-Air.local>
2022-06-13 21:17:45 +0900

    display-message

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,25 +1,29 @@
 import React, {useEffect, useState} from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import { List } from '@material-ui/core';
 import { messagesRef } from "../firebase";
+import MessageItem from "./MessageItem";
 
 const useStyles = makeStyles({
     root: {
         gridRow: 1,
+        overflow: 'auto',
+        width: '100%'
     },
 })
 
 const MessageList = () => {
     const classes = useStyles();
-    const [messages, setMessage] = useState('');
+    const [messages, setMessage] = useState([]);
 
     useEffect(() => {
         messagesRef
         .orderByKey()
-        .limitToLast(3)
+        .limitToLast(15)
         .on('value', (snapshot) => {
             const message = snapshot.val();
             if (message === null) return;
-            const entries = Object.entries(snapshot.val())
+            const entries = Object.entries(message)
             const newMessages = entries.map((entry) => {
                 const [key, nameAndText] = entry;
                 return {key, ...nameAndText};
@@ -28,7 +32,15 @@ const MessageList = () => {
         });
     }, [])
 
-    return <div className={classes.root}>MessageList</div>
+    return (<List className={classes.root}>
+            {
+                messages.map(({key, name, text}) => {
+                    return <MessageItem key={key} name={name} text={text}>{text}</MessageItem>
+                })
+            }
+        </List>
+    )
+    
 }
 
 export default MessageList;
